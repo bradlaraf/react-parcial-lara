5ed81bb2eda3b55dde85d2c94255c131dc776c7e
brad <bradlara7@gmail.com>
2023-06-01 13:50:51 -0500

    test

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,8 @@ function App() {
     <>
       <h1>Ideas Digitales</h1>
       <h2>Sistema web isil</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit consequatur laudantium incidunt optio libero dignissimos, velit recusandae beatae reprehenderit similique est sequi illo nesciunt quia fugiat sunt, nobis saepe eveniet!
+      Dignissimos ipsum odio nihil dolorem quos, animi facilis iste cum nesciunt unde! Alias voluptate placeat quos repellat accusamus! Inventore ipsa tenetur praesentium, dignissimos alias voluptatem esse quia? Dicta, ipsum architecto.</p>
     </>
   )
 }
